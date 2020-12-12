3318644815c4294a19d698890965120329eedb9f
Artem Naumenko <artem.naumenko@skyeng.ru>
2020-12-12 16:52:21 +0300

    CI deploy/revert

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-	<h1>This is update</h1>
+	<h1>Test of revert</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
