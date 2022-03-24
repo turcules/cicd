4e781bfebac5e636f70c57b8d8cb330105b5d645
root <root@instance-1.us-central1-a.c.ordinal-thinker-332311.internal>
2022-03-24 17:41:33 +0000

    Updatethird

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 	<h1>Test of revert</h1>
         <p>
-            third
+            five
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
